1bedee84333d7ed0d85fe03b0789289f22c201c0
klmhyeonwoo <gentlemonster77@likelion.org>
2025-09-23 23:47:21 +0900

    feat: Added individual package export method

--- package/index.ts
+++ package/index.ts
@@ -1,3 +1,4 @@
+// 네임스페이스에 대한 익스포트 진행
 export * as stringUtil from "./stringUtil";
 export * as objectUtil from "./objectUtil";
 export * as cookieUtil from "./cookieUtil";
@@ -5,3 +6,14 @@ export * as numberUtil from "./numberUtil";
 export * as validationUtil from "./validationUtil";
 export * as commonUtil from "./commonUtil";
 export * as searchQueryUtil from "./searchQueryUtil";
+export * as typeUtil from "./typeUtil";
+
+// 개별 함수에 대한 익스포트 진행
+export * from "./stringUtil";
+export * from "./objectUtil";
+export * from "./cookieUtil";
+export * from "./numberUtil";
+export * from "./validationUtil";
+export * from "./commonUtil";
+export * from "./searchQueryUtil";
+export * from "./typeUtil";
