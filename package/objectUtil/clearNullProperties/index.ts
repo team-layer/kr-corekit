a8f9d0ea715ca85260c90d1ad73cd43194826881
klmhyeonwoo <gentlemonster77@likelion.org>
2025-09-23 23:47:58 +0900

    fix: Fix bug in clearNullProperties utility function

--- package/objectUtil/clearNullProperties/index.ts
+++ package/objectUtil/clearNullProperties/index.ts
@@ -1,11 +1,18 @@
+import isPlainObject from "../../typeUtil/isPlainObject";
+
 export default function clearNullProperties<T>(obj: T): T {
-    const result = { ...obj };
-    Object.keys(result as Record<string, any>).forEach((el) => {
-        if (!(result as Record<string, any>)[el]) {
-            delete (result as Record<string, any>)[el];
-        } else if (typeof (result as Record<string, any>)[el] === "object") {
-            (result as Record<string, any>)[el] = clearNullProperties((result as Record<string, any>)[el]);
-        }
-    })
-    return result;
+  const result = { ...obj };
+  Object.keys(result as Record<string, any>).forEach((el) => {
+    if (
+      (result as Record<string, any>)[el] === null ||
+      (result as Record<string, any>)[el] === undefined
+    ) {
+      delete (result as Record<string, any>)[el];
+    } else if (isPlainObject((result as Record<string, any>)[el])) {
+      (result as Record<string, any>)[el] = clearNullProperties(
+        (result as Record<string, any>)[el]
+      );
+    }
+  });
+  return result;
 }
